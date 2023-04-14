9deda24b90d7eb26cc75b4c13b6f7aebd0dca8fc
Anastasiia <anastasiia.toseva@gmail.com>
2023-04-14 12:05:56 +0300

    'Test'

--- FinalProjectDominos/src/main/java/ittalents/dominos/DominosApplication.java
+++ FinalProjectDominos/src/main/java/ittalents/dominos/DominosApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DominosApplication {
-    //test
     public static void main(String[] args) {
         SpringApplication.run(DominosApplication.class, args);
     }
