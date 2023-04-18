2688d8c2914c30c8f7e75def129f0887c7983d92
nayme <naymeto25@gmail.com>
2023-04-18 21:44:30 +0300

    edit ProductController

--- FinalProjectDominos/src/main/java/ittalents/dominos/controller/ProductController.java
+++ FinalProjectDominos/src/main/java/ittalents/dominos/controller/ProductController.java
@@ -3,9 +3,6 @@ package ittalents.dominos.controller;
 import ittalents.dominos.model.DTOs.ProductDTO;
 import ittalents.dominos.model.DTOs.ProductEditDTO;
 import ittalents.dominos.model.DTOs.ProductWithoutImageDTO;
-import ittalents.dominos.model.entities.Product;
-import ittalents.dominos.model.exceptions.NotFoundException;
-import ittalents.dominos.model.exceptions.UnauthorizedException;
 import ittalents.dominos.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,45 +10,33 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class ProductController extends AbstractController {
     @Autowired
     private ProductService productService;
     @PostMapping("/dominos/products")
-    public ProductWithoutImageDTO addIngredient(@RequestBody ProductWithoutImageDTO dto, HttpSession s) {
-        if (isAdminLoggedIn(s)) {
-            //int loggedId = getLoggedId(s);
-            return productService.addProduct(dto);
-        } else{
-            throw new UnauthorizedException("ala bala");
-        }
+    public ProductWithoutImageDTO addProduct(@RequestBody ProductWithoutImageDTO dto, HttpSession s) {
+        isAdminLoggedIn(s);
+        return productService.addProduct(dto);
     }
 
     @DeleteMapping("/dominos/products/{id}")
     public void deleteProduct(@PathVariable("id") int id, HttpSession s){
-        if(isAdminLoggedIn(s)){
-            Product product = productService.getProductById(id);
-            if(product != null){
-                productService.deleteProduct(id);
-            }
-        }else{
-            throw new NotFoundException("not found");
-        }
+        isAdminLoggedIn(s);
+        productService.deleteProduct(id);
     }
 
-    @GetMapping("/dominos/products/{id}")
+    @GetMapping("/dominos/products/{id}/details")
     public ProductDTO viewProduct(@PathVariable("id") int id){
         ProductDTO productDTO = productService.viewProduct(id);
         return productDTO;
     }
 
     @PutMapping("/dominos/products/{id}")
-    public void editProduct(ProductEditDTO dto,@PathVariable("id") int id, HttpSession s){
-        if(isAdminLoggedIn(s)){
-            Product product = productService.getProductById(id);
-            if(product != null){
-                productService.editProduct(dto, id);
-            }
-        }
+    public ProductEditDTO editProduct(ProductEditDTO dto,@PathVariable("id") int id, HttpSession s){
+        isAdminLoggedIn(s);
+        ProductEditDTO product = productService.editProduct(dto, id);
+        return product;
     }
 
     @GetMapping("/dominos/products/{categoryId}")
