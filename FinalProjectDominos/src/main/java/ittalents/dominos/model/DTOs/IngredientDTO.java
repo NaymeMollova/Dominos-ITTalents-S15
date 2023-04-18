59e3e2cb1dd098effa41921765dfde896617f9be
Anastasiia <anastasiia.toseva@gmail.com>
2023-04-18 19:04:54 +0300

    Edit IngredientController, IngredientDTO.java, OrderInfoDTO.java,IngredientRepository.java, IngredientService.java

--- FinalProjectDominos/src/main/java/ittalents/dominos/model/DTOs/IngredientDTO.java
+++ FinalProjectDominos/src/main/java/ittalents/dominos/model/DTOs/IngredientDTO.java
@@ -1,5 +1,6 @@
 package ittalents.dominos.model.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class IngredientDTO {
 
     private String name;
